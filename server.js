37ca8d9c39fe9f583880f3d75921928a1d69f50a
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-03-07 21:51:21 -0800

    got server.js file started

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const fs = require("fs");
+const notes = require("./Develop/db/db.json")
+const path = require("path");
+
+const app = express();
+const PORT = 3001;
+
+
+
